01397f14168048308d6d4d15c36cb48e492beb79
Tatiana <tsg1204@gmail.com>
2019-03-08 10:27:39 -0500

    add summary to dashboard

---
+++ src/selectors/expenses-total.js
@@ -0,0 +1,12 @@
+export default (expenses) => {
+    return expenses
+        .map((expense) => expense.amount)
+        .reduce((sum, value) => sum + value, 0);    
+    // if (expenses.length === 0) {
+    //     return 0;
+    // } else {
+    //     return expenses
+    //         .map((expense) => expense.amount)
+    //         .reduce((sum, value) => sum + value, 0);
+    // }
+};
